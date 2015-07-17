f08bd4965ddccbebe858a4deffe1ab919c660afe
Juan Farias <juan_farias@artear.com>
2015-07-17 16:16:26 -0300

    test wpt

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+/*
+var WebPageTest = require('webpagetest');
+
+var wpt = new WebPageTest('www.webpagetest.org', 'A.559d4ae5af277d98b7ba0857515714cd');
+
+var config = {"connectivity":"3G",
+              "location":"Dulles_MotoG:Motorola G - Chrome",
+              "runs":"3"};
+              
+              //login password
+
+wpt.runTest('m.tn.com.ar', config, function callback(err, data) {
+  console.log(err || data);
+});
+*/
+
 io.on('connection', function (socket) {
 
   socket.emit('connected', { message: 'hello new client!' });
