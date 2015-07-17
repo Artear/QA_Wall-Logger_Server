ca68169d3aa62cf01b4d9f888f76155737916d93
adaraio <alejo_daraio@artear.com>
2015-07-17 13:34:17 -0300

    change socket port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8080);
+server.listen(9187);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
