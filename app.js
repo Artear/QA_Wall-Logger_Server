9b03abc13d05a3cc3f953e0156565bf2b556e564
adaraio <alejo_daraio@artear.com>
2015-07-17 13:46:23 -0300

    changed messages

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+
+  socket.emit('connected', { message: 'hello new client!' });
+
+  socket.on('message', function (data) {
     console.log(data);
   });
+
 });
