301091e4d8502905478a6b620129566fda21114a
Juan Farias <juan_farias@artear.com>
2015-10-06 18:03:54 -0300

    added bitly

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var app = require('express')(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
-    psi = require('psi'),
     WebPageTest = require('webpagetest'),
-    wpt = new WebPageTest('www.webpagetest.org', 'A.559d4ae5af277d98b7ba0857515714cd');
+    wpt = new WebPageTest('www.webpagetest.org', 'A.559d4ae5af277d98b7ba0857515714cd'),
+    bitlyAPI = require("node-bitlyapi"),
+    bitly = new bitlyAPI({
+      client_id: "3d4a4e7c2c8cfa6e6d357f93bc83a13220feb899",
+      client_secret: "0404717faef381a7d60ad7a0d3aca3ffedbf5373"
+    });
 
+
+bitly.setAccessToken('b8e564b879029ff16c9c08f3b212affbb60f7ec7');
 /**
  * globals
  */
@@ -61,6 +67,13 @@ io.on('connection', function (socket) {
     //request page speed
     //startPSI(data, socket);
     startWPT(data, socket);
+
+    var url = 'https://developers.google.com/speed/pagespeed/insights/?url=' + encodeURIComponent(data.url)
+
+    bitly.shorten({longUrl:url}, function(err, results) {
+      results = JSON.parse(results);
+      socket.emit('bitly.psi', results.data.url);
+    });
   });
 
   socket.on('reset', function(data){
@@ -95,5 +108,10 @@ function startWPT(data, socket){
     socket.emit('wpt', wptdata.data);
 
     console.log('WPT status:', err || wptdata);
+
+    bitly.shorten({longUrl:wptdata.userUrl}, function(err, results) {
+      results = JSON.parse(results);
+      socket.emit('bitly.wpt', results.data.url);
+    });
   });
 }
