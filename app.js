1ea7c5746a8537fc666894479510dae89041fb52
Juan Farias <juan_farias@artear.com>
2015-11-26 17:49:48 -0300

    fix js

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ var app = require('express')(),
     });
 bitly.setAccessToken('b8e564b879029ff16c9c08f3b212affbb60f7ec7');
 
+// https://www.npmjs.com/package/shelljs#exec-command-options-callback
 require('shelljs/global');
-
-
 exec('ls', {silent:true}, function(code, output) {
   console.log('Exit code:', code);
   console.log('Program output:', output);
