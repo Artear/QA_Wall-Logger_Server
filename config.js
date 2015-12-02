a56c10e224c3674983273dfcc19c75936e8ac46e
Martin Moreno <155martinmoreno@gmail.com>
2015-12-02 12:51:12 -0300

    Added: config default; Ignored: config file;

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.static_dir = "../QA_Wall-Data_Analysis";
-config.static_port = 8081;
+config.static_dir = "../QA_Wall-Data_Analysis";//Static Server doc_root
+config.static_port = 8081;//Static Server port
 
 module.exports = config;
