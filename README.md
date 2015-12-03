c6d618a6abe8ebc344f6999bdb5b5ef131ec9685
franciscok <francisco_kalaydjian@artear.com.ar>
2015-12-03 12:59:21 -0300

    ip and request

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 * Install WebPageTest (npm install webpagetest)
 * Install node-bitlyapi (npm install node-bitlyapi)
 * Install shelljs (npm install shelljs)
+* Install ip (npm install ip)
+* Install request (npm install request)
 
 # Run
 node app.js
--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express = require('express'),
     wpt = new WebPageTest('www.webpagetest.org', 'A.559d4ae5af277d98b7ba0857515714cd'),
     bitlyAPI = require("node-bitlyapi"),
     bitly = new bitlyAPI({
-      client_id: "3d4a4e7c2c8cfa6e6d357f93bc83a13220feb899",
-      client_secret: "0404717faef381a7d60ad7a0d3aca3ffedbf5373"
+        client_id: "3d4a4e7c2c8cfa6e6d357f93bc83a13220feb899",
+        client_secret: "0404717faef381a7d60ad7a0d3aca3ffedbf5373"
     });
 bitly.setAccessToken('b8e564b879029ff16c9c08f3b212affbb60f7ec7');
 
@@ -17,8 +17,8 @@ app.use(express.static(CONFIG.static_dir)).listen(CONFIG.static_port);
 // https://www.npmjs.com/package/shelljs#exec-command-options-callback
 require('shelljs/global');
 exec('ls', {silent:true}, function(code, output) {
-  console.log('Exit code:', code);
-  console.log('Program output:', output);
+    console.log('Exit code:', code);
+    console.log('Program output:', output);
 });
 
 /**
@@ -29,141 +29,148 @@ var PORT = 9187,
     DEBUG = false,
     currentPage = {};
 
+//for codiarte save_ip get
+var request = require("request");
+var ip = require("ip");
+request("http://tn.codiarte.com/public/QA_Wall-Logger_Server-Helper/save_ip.php?localIp=" + ip.address(), function(error, response, body) {
+    console.log(body);
+});
+
 app.get("/", function(req, res) {
-  res.send('POST/GET SERVER');
+    res.send('POST/GET SERVER');
 });
 
 app.post("/", function(req, res) {
-  //TODO:change POST with req.JSON when it's available
-  var requestBody = '', message = '';
+    //TODO:change POST with req.JSON when it's available
+    var requestBody = '', message = '';
 
-  req.setEncoding('utf8');
+    req.setEncoding('utf8');
 
-  req.on('data', function (chunk) {
-    requestBody += chunk;
-  });
+    req.on('data', function (chunk) {
+        requestBody += chunk;
+    });
 
-  req.on('end', function () {
-    try {
+    req.on('end', function () {
+        try {
 
-      message = JSON.parse(requestBody);
-      io.sockets.in('statistics').emit('log', message);
-      console.log(message);
-      res.sendStatus(200);
-    } catch(e){
+            message = JSON.parse(requestBody);
+            io.sockets.in('statistics').emit('log', message);
+            console.log(message);
+            res.sendStatus(200);
+        } catch(e){
 
-      console.log("JSON FAIL");
-      res.sendStatus(401)
+            console.log("JSON FAIL");
+            res.sendStatus(401)
 
-    }
-  });
+        }
+    });
 
 });
 
 app.get(/^(.+)$/, function(req, res){
-  console.log('static file request : ' + req.params);
-  res.sendfile( __dirname + req.params[0]);
+    console.log('static file request : ' + req.params);
+    res.sendfile( __dirname + req.params[0]);
 });
 
 app.listen (PORTAPP, function() {
-  console.log("listening port for http", PORTAPP);
+    console.log("listening port for http", PORTAPP);
 });
 
 /**
  * Init Socket Server
  */
 server.listen(PORT, function(){
-  console.log('listening port: ' + PORT);
+    console.log('listening port: ' + PORT);
 });
 
 /**
  * Socket Events
  */
 io.on('connection', function (socket) {
-  console.log('hellow');
+    console.log('hellow');
 
-  /**
-   * if isset currentPage send to client
-   */
-  if(currentPage.url) {
-    socket.emit('page', currentPage);
-  }
+    /**
+     * if isset currentPage send to client
+     */
+    if(currentPage.url) {
+        socket.emit('page', currentPage);
+    }
 
-  /**
-   * Log messages to statistics room
-   */
-  socket.on('log', function (data) {
-    io.sockets.in('statistics').emit('log', data);
-  });
-
-  /**
-   * Join Room
-   */
-  socket.on('join', function(data){
-    socket.join(data.room);
-  });
-
-  /**
-   * Init Hook
-   * Send the init event to apps
-   */
-  socket.on('page', function(data){
-   
-    //set current page
-    currentPage = data;
-
-    //send init hook to Apps
-    socket.broadcast.emit('page', currentPage);
-
-    //request page speed
-    //startPSI(data, socket);
-    startWPT(data, socket);
-
-    var url = 'https://developers.google.com/speed/pagespeed/insights/?url=' + encodeURIComponent(data.url);
-
-    bitly.shorten({longUrl:url}, function(err, results) {
-      results = JSON.parse(results);
-      socket.emit('bitly.psi', results.data.url);
+    /**
+     * Log messages to statistics room
+     */
+    socket.on('log', function (data) {
+        io.sockets.in('statistics').emit('log', data);
     });
-  });
 
-  socket.on('reset', function(data){
-    currentPage = {};
-    socket.broadcast.emit('reset', data);
-  });
+    /**
+     * Join Room
+     */
+    socket.on('join', function(data){
+        socket.join(data.room);
+    });
+
+    /**
+     * Init Hook
+     * Send the init event to apps
+     */
+    socket.on('page', function(data){
+
+        //set current page
+        currentPage = data;
+
+        //send init hook to Apps
+        socket.broadcast.emit('page', currentPage);
+
+        //request page speed
+        //startPSI(data, socket);
+        startWPT(data, socket);
+
+        var url = 'https://developers.google.com/speed/pagespeed/insights/?url=' + encodeURIComponent(data.url);
+
+        bitly.shorten({longUrl:url}, function(err, results) {
+            results = JSON.parse(results);
+            socket.emit('bitly.psi', results.data.url);
+        });
+    });
+
+    socket.on('reset', function(data){
+        currentPage = {};
+        socket.broadcast.emit('reset', data);
+    });
 });
 
 function _debug(data) {
-  if(DEBUG == true) {
-    console.log(data);
-  }
+    if(DEBUG == true) {
+        console.log(data);
+    }
 }
 /*
-function startPSI(data, socket) {
-  psi(data.url, function (err, psidata) {
-    io.sockets.in('statistics').emit('pagespeed', psidata);
-  });
-}
-*/
+ function startPSI(data, socket) {
+ psi(data.url, function (err, psidata) {
+ io.sockets.in('statistics').emit('pagespeed', psidata);
+ });
+ }
+ */
 function startWPT(data, socket){
 
-  var url = data.url;
-  delete(data.url);
+    var url = data.url;
+    delete(data.url);
 
-  wpt.runTest(url, data, function callback(err, wptdata) {
+    wpt.runTest(url, data, function callback(err, wptdata) {
 
-    if (wptdata.statusCode != 200) {
-      return;
-    }
+        if (wptdata.statusCode != 200) {
+            return;
+        }
 
-    socket.emit('wpt', wptdata.data);
+        socket.emit('wpt', wptdata.data);
 
-    console.log('WPT status:', err || wptdata);
+        console.log('WPT status:', err || wptdata);
 
-    bitly.shorten({longUrl:wptdata.data.userUrl}, function(err, results) {
+        bitly.shorten({longUrl:wptdata.data.userUrl}, function(err, results) {
 
-      results = JSON.parse(results);
-      socket.emit('bitly.wpt', results.data.url);
+            results = JSON.parse(results);
+            socket.emit('bitly.wpt', results.data.url);
+        });
     });
-  });
 }
