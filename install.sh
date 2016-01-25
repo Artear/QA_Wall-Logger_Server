79be497f1e04035a7e37b87702a12424a67477ef
QA Wall <qawall@QAs-Mac-mini.local>
2016-01-25 18:02:14 -0300

    Changed: install script;

--- install.sh
+++ install.sh
@@ -5,18 +5,23 @@ ANDROID_SDK_NAME="android-sdk_r24.4.1-macosx"
 ANDROID_SDK_ZIP_NAME="$ANDROID_SDK_NAME.zip"
 ANDROID_SDK_DOWNLOAD_SOURCE="http://dl.google.com/android/$ANDROID_SDK_ZIP_NAME"
 ANDROID_SDK_DOWNLOAD_DESTINATION="$TMP_DIR/$ANDROID_SDK_ZIP_NAME"
-ANDROID_SDK_FOLDER="$HOME/Library/"
-ANDROID_TOOLS_PATH="$ANDROID_SDK_FOLDER/android-sdk-macosx/tools/android"
+ANDROID_SDK_FOLDER="$HOME/Library"
+ANDROID_TOOLS_PATH="$ANDROID_SDK_FOLDER/android-sdk-macosx/tools"
 ANDROID_PLATFORM_TOOLS_PATH="$ANDROID_SDK_FOLDER/android-sdk-macosx/platform-tools"
 
 
-echo " ************************"
-echo "*QA_Wall Server Isntaller*"
-echo " ************************"
+echo " **************************"
+echo "* QA_Wall Server Isntaller *"
+echo " **************************"
 echo ""
 echo "Disclaimer: this is a VERY simple install script, no robust checks are in place. If this script fails, you can mannualy install all components."
 echo ""
 echo "** You need to have Xcode insalled for this to work!"
+echo ""
+echo "** You need to have a JDK insalled for this to work!"
+echo ""
+echo ""
+read -p "Only continue if you have the above installed! (Press any key to continue...)"
 echo ""
 echo ""
 echo ""
@@ -48,16 +53,27 @@ curl "$ANDROID_SDK_DOWNLOAD_SOURCE" -o "$ANDROID_SDK_DOWNLOAD_DESTINATION"
 echo ""
 echo "Unziping into $ANDROID_SDK_FOLDER..."
 mkdir "$ANDROID_SDK_FOLDER"
-unzip "$ANDROID_SDK_DOWNLOAD_DESTINATION" -d "$ANDROID_SDK_FOLDER"
+unzip -o "$ANDROID_SDK_DOWNLOAD_DESTINATION" -d "$ANDROID_SDK_FOLDER"
 
-export PATH=$PATH:$ANDROID_TOOLS_PATH
 
-android update sdk --filter platform-tools
+$ANDROID_TOOLS_PATH/android update sdk --filter platform-tools
 
-export PATH=$PATH:$ANDROID_PLATFORM_TOOLS_PATH
 
 echo ""
 echo ""
+
 echo "Cleaning temp dir..."
 rm -R "$TMP_DIR"
 
+echo ""
+echo ""
+echo ""
+echo "******************* ATTENTION! **************************"
+echo "     Please add these to your .bash_profile "
+echo "           export PATH=$ANDROID_TOOLS_PATH:\$PATH"
+echo "           export PATH=$ANDROID_PLATFORM_TOOLS_PATH:\$PATH"
+echo "*********************************************************"
+echo ""
+echo ""
+echo ""
+echo ""
