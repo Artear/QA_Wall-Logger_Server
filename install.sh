8dc0c403a1d76641f7129b341eb0f321cbafd841
Martin Moreno <155martinmoreno@gmail.com>
2016-01-25 15:24:17 -0300

    Changed: install script;

--- install.sh
+++ install.sh
@@ -10,31 +10,31 @@ ANDROID_TOOLS_PATH="$ANDROID_SDK_FOLDER/android-sdk-macosx/tools/android"
 ANDROID_PLATFORM_TOOLS_PATH="$ANDROID_SDK_FOLDER/android-sdk-macosx/platform-tools"
 
 
-#echo " ************************"
-#echo "*QA_Wall Server Isntaller*"
-#echo " ************************"
-#echo ""
-#echo "Disclaimer: this is a VERY simple install script, no robust checks are in place. If this script fails, you can mannualy install all components."
-#echo ""
-#echo "** You need to have Xcode insalled for this to work!"
-#echo ""
-#echo ""
-#echo ""
-#
-#echo "Installing Homebrew (if needed)..."
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#
-#echo ""
-#echo "Installing Node..."
-#brew install node
-#
-#echo ""
-#echo "Installing Xcode command line tools..."
-#xcode-select --install
-#
-#echo ""
-#echo "Installing Project dependencies..."
-#npm install
+echo " ************************"
+echo "*QA_Wall Server Isntaller*"
+echo " ************************"
+echo ""
+echo "Disclaimer: this is a VERY simple install script, no robust checks are in place. If this script fails, you can mannualy install all components."
+echo ""
+echo "** You need to have Xcode insalled for this to work!"
+echo ""
+echo ""
+echo ""
+
+echo "Installing Homebrew (if needed)..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo ""
+echo "Installing Node..."
+brew install node
+
+echo ""
+echo "Installing Xcode command line tools..."
+xcode-select --install
+
+echo ""
+echo "Installing Project dependencies..."
+npm install
 
 echo ""
 echo "Creating temp dir $TMP_DIR..."
